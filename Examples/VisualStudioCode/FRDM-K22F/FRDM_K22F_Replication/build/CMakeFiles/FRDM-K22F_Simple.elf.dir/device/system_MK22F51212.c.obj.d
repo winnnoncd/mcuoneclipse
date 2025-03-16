c9b37c925202e1a5e9ae19c17582d3822d5c9588
Canh-Son Nguyen <winnoncd@gmail.com>
2025-03-16 18:59:53 +0700

    Build binary - output after make command

---
+++ Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/build/CMakeFiles/FRDM-K22F_Simple.elf.dir/device/system_MK22F51212.c.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/FRDM-K22F_Simple.elf.dir/device/system_MK22F51212.c.obj: \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/device/system_MK22F51212.c \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/device/fsl_device_registers.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/device/MK22F51212.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/CMSIS/core_cm4.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/CMSIS/cmsis_version.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/CMSIS/cmsis_compiler.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/CMSIS/cmsis_gcc.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/device/system_MK22F51212.h \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/device/MK22F51212_features.h
---
+++ Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/build/CMakeFiles/FRDM-K22F_Simple.elf.dir/src/main.c.obj.d
@@ -0,0 +1,27 @@
+CMakeFiles/FRDM-K22F_Simple.elf.dir/src/main.c.obj: \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/src/main.c \
+ D:/Git_repos/mcuoneclipse/Examples/VisualStudioCode/FRDM-K22F/FRDM_K22F_Replication/src/main.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\stdio.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\newlib.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\config.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\cdefs.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\reent.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\_types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\lock.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\machine\types.h \
+ c:\tools\gnu\ arm\ embedded\ toolchain\10\ 2021.10\arm-none-eabi\include\sys\stdio.h
